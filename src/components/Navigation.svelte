5317019ff086b85d0e72d977c14334076917eb8e
AJ Caldwell <tomatrow@gmail.com>
2020-12-05 19:59:46 -0800

    Add proper font to the nav items

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -3,6 +3,14 @@
     export let items
 </script>
 
+<style>
+    span {
+        font-family: "Bauhaus 93";
+    }
+</style>
+
 {#each items as { active, externalLink }}
-    <Link href={externalLink.url}><span class:font-bold={active}>{externalLink.title}</span></Link>
+    <Link href={externalLink.url}>
+        <span class:font-bold={active} class="uppercase">{externalLink.title}</span>
+    </Link>
 {/each}
