82687e34c4e26bce778e79be632dbfe1aae95deb
AJ Caldwell <tomatrow@gmail.com>
2020-12-20 17:20:45 -0800

    Make testimonials on / retate based on day of the year

--- src/routes/Home/Testimonials.svelte
+++ src/routes/Home/Testimonials.svelte
@@ -1,14 +1,16 @@
 <script>
+    import getDayOfYear from "date-fns/getDayOfYear"
     export let items
+    const item = items?.length && items[getDayOfYear(new Date()) % items.length]
 </script>
 
-<div class="space-y-8 flex flex-col">
-    {#each items as item}
+{#if item}
+    <div class="space-y-8 flex flex-col">
         <div class="sm:space-x-4 flex items-center flex-col sm:flex-row justify-center">
             <div class="space-y-4 flex flex-col font-bold">
                 {@html item.excerpt}
                 <h3 class="font-bauhaus uppercase text-xl">{item.title}</h3>
             </div>
         </div>
-    {/each}
-</div>
+    </div>
+{/if}
