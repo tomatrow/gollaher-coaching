40b7452287360111ab1a62ed2d2e2314b720c8a5
AJ Caldwell <tomatrow@gmail.com>
2021-06-20 15:49:01 -0700

    Add control for best value badge

--- src/routes/Home/Packages.svelte
+++ src/routes/Home/Packages.svelte
@@ -1,17 +1,11 @@
 <script>
     import Card from "./Card.svelte"
     export let items
+    console.log({ items })
 </script>
 
-<style>
-    .badge {
-        top: -2.25rem;
-        right: -1.5rem;
-    }
-</style>
-
 <div class="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
-    {#each items as { excerpt, customContent: { name, price } }, i}
+    {#each items as { excerpt, customContent: { name, price, isBestValue } }}
         <Card class="relative px-4 rounded text-white">
             <div class="py-4">
                 <div>
@@ -21,10 +15,11 @@
                     {@html excerpt}
                 </div>
             </div>
-            {#if i === 0}
+            {#if isBestValue}
                 <img
                     class="badge absolute z-10 w-16 h-16"
                     src="/assets/star.png"
+                    style="top: -2.25rem; right: -1.5rem;"
                     alt="best value" />
             {/if}
         </Card>
